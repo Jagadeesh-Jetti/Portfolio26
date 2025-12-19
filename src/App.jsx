df10243bccca8c1abee56495b9a436d114301801
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-12-19 11:26:01 +0530

    feat: initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TopHeader } from './components/header/TopHeader';
+import { BottomNav } from './components/navigation/BottomNav';
+
+function App() {
+  return (
+    <React.Fragment>
+      <TopHeader />
+
+      <BottomNav />
+    </React.Fragment>
+  );
+}
+
+export default App;
