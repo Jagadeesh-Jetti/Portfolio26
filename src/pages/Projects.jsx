bc1fd8bc725d01650057e37dbfea158a6aaf4a75
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-12-19 21:37:25 +0530

    refactor: updated code component

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,29 @@
+import { projectsData } from '../data/projectsData';
+
 export const Projects = () => {
   return (
     <div className="m-5">
       <h1 className="font-bold ml-1"> Projects</h1>
-      <div>
+      {projectsData.map((project) => (
         <div className=" m-2 p-5 border-none border-amber-950 shadow-md">
-          <div className="text-lg font-medium">
-            FocusFlow - AI powered Productivity Planner
+          <div className="text-lg font-medium">{project?.title}</div>
+          <div className="flex">
+            <div className="m-2 p-1 border text-sm p-2,2"> Visit </div>
+            <div className="m-2 p-1 border text-sm p-2,2"> Code </div>
           </div>
+          <div className="text-gray-500">{project?.description}</div>
+          <div className="flex justify-around m-3">
+            {project.tech.map((tech) => (
+              <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+                {tech}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+      {/* <div>
+        <div className=" m-2 p-5 border-none border-amber-950 shadow-md">
+          <div className="text-lg font-medium">{title}</div>
           <div className="flex">
             <div className="m-2 p-1 border text-sm p-2,2"> Visit </div>
             <div className="m-2 p-1 border text-sm p-2,2"> Code </div>
@@ -16,6 +33,36 @@ export const Projects = () => {
             stack, designed to help users set goals, plan tasks, and stay
             consistent. It features secure authentication, full CRUD task
             management, Redux-based state handling, and a clean,
+            distraction-free UI styled with Tailwind CSS. {description}
+          </div>
+          <div className="flex justify-around m-3">
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              HTML
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Javacript
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              React
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Tailwindcss
+            </div>
+          </div>
+        </div> */}
+      {/* <div className="m-2 p-5 border-none border-amber-950 shadow-md">
+          <div className="text-lg font-bold">
+            FocusFlow - AI powered Productivity Planner
+          </div>
+          <div className="flex">
+            <div className="m-2"> visit </div>
+            <div className="m-2"> code </div>
+          </div>
+          <div className="text-gray-500">
+            FocusFlow is an AI-powered productivity planner built with the MERN
+            stack, designed to help users set goals, plan tasks, and stay
+            consistent. It features secure authentication, full CRUD task
+            management, Redux-based state handling, and a clean,
             distraction-free UI styled with Tailwind CSS.
           </div>
           <div className="flex justify-around m-3">
@@ -32,8 +79,8 @@ export const Projects = () => {
               Tailwindcss
             </div>
           </div>
-        </div>
-        <div className="m-2 p-5 border-none border-amber-950 shadow-md">
+        </div> */}
+      {/* <div className="m-2 p-5 border-none border-amber-950 shadow-md">
           <div className="text-lg font-bold">
             FocusFlow - AI powered Productivity Planner
           </div>
@@ -63,37 +110,7 @@ export const Projects = () => {
             </div>
           </div>
         </div>
-        <div className="m-2 p-5 border-none border-amber-950 shadow-md">
-          <div className="text-lg font-bold">
-            FocusFlow - AI powered Productivity Planner
-          </div>
-          <div className="flex">
-            <div className="m-2"> visit </div>
-            <div className="m-2"> code </div>
-          </div>
-          <div className="text-gray-500">
-            FocusFlow is an AI-powered productivity planner built with the MERN
-            stack, designed to help users set goals, plan tasks, and stay
-            consistent. It features secure authentication, full CRUD task
-            management, Redux-based state handling, and a clean,
-            distraction-free UI styled with Tailwind CSS.
-          </div>
-          <div className="flex justify-around m-3">
-            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
-              HTML
-            </div>
-            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
-              Javacript
-            </div>
-            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
-              React
-            </div>
-            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
-              Tailwindcss
-            </div>
-          </div>
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 };
