3d18713ec159ba425b94441862e4aeb8958cf294
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-12-19 15:51:22 +0530

    feat: created projects component

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,99 @@
+export const Projects = () => {
+  return (
+    <div className="m-5">
+      <h1 className="font-bold ml-1"> Projects</h1>
+      <div>
+        <div className=" m-2 p-5 border-none border-amber-950 shadow-md">
+          <div className="text-lg font-medium">
+            FocusFlow - AI powered Productivity Planner
+          </div>
+          <div className="flex">
+            <div className="m-2 p-1 border text-sm p-2,2"> Visit </div>
+            <div className="m-2 p-1 border text-sm p-2,2"> Code </div>
+          </div>
+          <div className="text-gray-500">
+            FocusFlow is an AI-powered productivity planner built with the MERN
+            stack, designed to help users set goals, plan tasks, and stay
+            consistent. It features secure authentication, full CRUD task
+            management, Redux-based state handling, and a clean,
+            distraction-free UI styled with Tailwind CSS.
+          </div>
+          <div className="flex justify-around m-3">
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              HTML
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Javacript
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              React
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Tailwindcss
+            </div>
+          </div>
+        </div>
+        <div className="m-2 p-5 border-none border-amber-950 shadow-md">
+          <div className="text-lg font-bold">
+            FocusFlow - AI powered Productivity Planner
+          </div>
+          <div className="flex">
+            <div className="m-2"> visit </div>
+            <div className="m-2"> code </div>
+          </div>
+          <div className="text-gray-500">
+            FocusFlow is an AI-powered productivity planner built with the MERN
+            stack, designed to help users set goals, plan tasks, and stay
+            consistent. It features secure authentication, full CRUD task
+            management, Redux-based state handling, and a clean,
+            distraction-free UI styled with Tailwind CSS.
+          </div>
+          <div className="flex justify-around m-3">
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              HTML
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Javacript
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              React
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Tailwindcss
+            </div>
+          </div>
+        </div>
+        <div className="m-2 p-5 border-none border-amber-950 shadow-md">
+          <div className="text-lg font-bold">
+            FocusFlow - AI powered Productivity Planner
+          </div>
+          <div className="flex">
+            <div className="m-2"> visit </div>
+            <div className="m-2"> code </div>
+          </div>
+          <div className="text-gray-500">
+            FocusFlow is an AI-powered productivity planner built with the MERN
+            stack, designed to help users set goals, plan tasks, and stay
+            consistent. It features secure authentication, full CRUD task
+            management, Redux-based state handling, and a clean,
+            distraction-free UI styled with Tailwind CSS.
+          </div>
+          <div className="flex justify-around m-3">
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              HTML
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Javacript
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              React
+            </div>
+            <div className="text-xs border-none border-gray-400 shadow-sm p-1">
+              Tailwindcss
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
