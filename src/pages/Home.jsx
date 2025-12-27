2ec8d9aeffa469559abd985584b08e34199d9b46
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2025-12-27 21:42:26 +0530

    refactor: refined code for home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ export const Home = () => {
               </div>
             </div>
           </div>
-          <div className="m-2">{post.content}</div>
+          <div className="m-1">{post.content}</div>
           <div>
             {post?.images?.length > 0 && (
               <div>
@@ -39,21 +39,6 @@ export const Home = () => {
           </div>
         </div>
       ))}
-
-      {/* <div className="mt-3 mb-1 border border-gray-300 p-4 bg-white">
-        <div className="flex gap-2">
-          <div className="w-11 h-11 border rounded-3xl"></div>
-          <div className="text-sm">
-            <div className="font-semibold"> Jagadeesh Jetti </div>
-            <div className="text-gray-500 text-xs"> Full Stack Developer </div>
-          </div>
-        </div>
-        <div className="m-2">
-          I just dumped my thoughts on AI revolution in to a blog and posted it.
-          Check it out in the blogs section
-        </div>
-        <div>Photos</div>
-      </div> */}
     </div>
   );
 };
