b67108f49f3b226573c8ae63c9b8bf858d5d6dce
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2026-01-02 23:59:22 +0530

    refactor: building blogs layout

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,8 +1,30 @@
 export const Blogs = () => {
   return (
-    <div>
-      <div>this is a blog </div>
-      <div>this is a blog</div>
+    <div className="ml-4 mr-4 bg-gray-100">
+      <div className="p-2">
+        <div className="text-2xl"> Blogs and Insights </div>
+        <p className="text-gray-700">
+          I write about building products, fixing mistakes, and thinking clearly
+          as a developer.
+        </p>
+      </div>
+      <div className="mt-3 p-2  bg-white">
+        <div>Featured</div>
+        <h2 className="text-xl">
+          Redesigning my portfolio to reduce cognitive load for recruiters
+        </h2>
+        <div>
+          <ul>
+            <li> Why i changed it </li>
+            <li> What didnt work </li>
+            <li> What finally clicked</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>More writings</div>
+        <div></div>
+      </div>
     </div>
   );
 };
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,20 @@
+const skills = [
+  'HTML',
+  'CSS',
+  'Javascript',
+  'React',
+  'Node.js',
+  'Tailwind',
+  'Express.js',
+  'MongoDB',
+];
+
 export const Skills = () => {
   return (
-    <div>
-      <div>this is a skill</div>
-      <div> this is a skill</div>
+    <div className="flex flex-wrap">
+      {skills.map((skill) => (
+        <div className="w-max h-10 m-1 p-2 bg-black text-white"> {skill} </div>
+      ))}
     </div>
   );
 };
